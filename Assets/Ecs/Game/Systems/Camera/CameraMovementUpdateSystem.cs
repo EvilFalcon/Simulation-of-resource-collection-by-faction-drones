09547ac4f402a86b54eff70fa49092641a3843a7
Maksim Sokolov <gitlab.thc.games>
2025-06-16 00:59:41 +0300

    Fix Bags Camera

--- Assets/Ecs/Game/Systems/Camera/CameraMovementUpdateSystem.cs
+++ Assets/Ecs/Game/Systems/Camera/CameraMovementUpdateSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Ecs.Core.Interfaces;
 using Ecs.Utils;
+using Ecs.Utils.Camera;
 using Game.Models.Camera;
 using InstallerGenerator.Attributes;
 using InstallerGenerator.Enums;
@@ -12,24 +13,22 @@ namespace Ecs.Game.Systems.Camera
     {
         private readonly ICameraHolder _cameraHolder;
         private readonly GameContext _gameContext;
-        private readonly ILinkedEntityRepository _linkedEntityRepository;
         private float panSpeed = 10f;
         private float edgeThreshold = 10f; // Расстояние от края экрана, при котором начинается движение
         private Vector3 mapMinBounds; // Минимальные границы карты (X, Y)
         private Vector3 mapMaxBounds; // Максимальные границы карты (X, Y)
 
-        public CameraMovementUpdateSystem(ICameraHolder cameraHolder, GameContext gameContext, ILinkedEntityRepository linkedEntityRepository)
+        public CameraMovementUpdateSystem(ICameraHolder cameraHolder, GameContext gameContext)
         {
             _cameraHolder = cameraHolder;
             _gameContext = gameContext;
-            _linkedEntityRepository = linkedEntityRepository;
         }
 
         public void Late()
         {
             Vector3 moveDirection = Vector3.zero;
             Vector2 mousePosition = UnityEngine.Input.mousePosition;
-            var camera = _cameraHolder.GetCamera();
+            var camera = _cameraHolder.GetVirtualCamera(EVirtualCameraType.Gameplay);
             var cameraPosition = camera.transform.position;
 
             // Проверяем, находится ли курсор у края экрана
@@ -38,33 +37,24 @@ namespace Ecs.Game.Systems.Camera
             else if (mousePosition.x >= Screen.width - edgeThreshold)
                 moveDirection.x = 1f; // Движение вправо
 
-            else if (mousePosition.y <= edgeThreshold)
+            if (mousePosition.y <= edgeThreshold)
                 moveDirection.z = -1f; // Движение вниз
             else if (mousePosition.y >= Screen.height - edgeThreshold)
                 moveDirection.z = 1f; // Движение вверх
-            else
-                return;
 
             // Нормализуем, если движение по диагонали
             if (moveDirection.magnitude > 1f)
                 moveDirection.Normalize();
 
-            Debug.Log("Движение камеры " + moveDirection + " и позиция курсора " + mousePosition);
-
             // Вычисляем новую позицию камеры
-            Vector3 newPosition = cameraPosition + (moveDirection * panSpeed);
+            Vector3 newPosition = cameraPosition + moveDirection * panSpeed * Time.deltaTime;
             var mapBounds = _gameContext.TerrainEntity.TerrainBounds;
             // Ограничиваем движение границами карты
             newPosition.x = Mathf.Clamp(newPosition.x, mapBounds.Min.x + 46.5f, mapBounds.Max.x - 46.5f);
             newPosition.z = Mathf.Clamp(newPosition.z, mapBounds.Min.z + 26, mapBounds.Max.z - 26);
 
-            Debug.Log(newPosition + " и позиция курсора " + mousePosition);
-
             // Применяем новую позицию
-            if (!_linkedEntityRepository.TryGet(camera.transform.GetHashCode(), out var cameraEntity))
-                return;
-
-           camera.transform.position = newPosition;
+            camera.transform.position = newPosition;
         }
     }
 }
